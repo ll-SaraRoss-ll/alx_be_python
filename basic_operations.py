11bfdd29dff4150578ab8e00cced8dd6e21c9d2b
Sara <sararossmay2000@gmail.com>
2025-07-08 17:21:29 +0300

    Add basic_operations.py for alx python instruction task

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+Substraction = number1 - number2
+Multiplication = number1 * number2
+print("the Addition of ", number1, "and ", number2, "is ", Addition)
+print("the Substraction of", number1, "and", number2, "is", Substraction)
+print("the Multiplication of", number1, "and", number2, "is", Multiplication)
