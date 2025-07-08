739d79fa5d4101b53f88af99748a8ac5fd8f988b
Sara <sararossmay2000@gmail.com>
2025-07-08 18:26:49 +0300

    Add updated version of finance_calculator.py for alx python introduction tasks

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,10 @@
-monthlyIncome = int(input("Enter your monthly income: "))
-monthlyExprenses = int(input("Enter your total monthly expenses: "))
-monthlySavings = monthlyIncome - monthlyExprenses
-interestRate = 0.05
-oneYear = 12
-ProjectedSavings = monthlySavings * oneYear + (monthlySavings * oneYear * interestRate)
-print("Your monthly savings are $",monthlySavings)
-print("Projected savings after one year, with interest, is: $",ProjectedSavings)
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+interest_rate = 0.05
+one_year = 12
+
+projected_savings = monthly_savings * one_year + (monthly_savings * one_year * interest_rate)
+
+print("Your monthly savings are $", monthly_savings)
+print("Projected savings after one year, with interest, is: $", projected_savings)
