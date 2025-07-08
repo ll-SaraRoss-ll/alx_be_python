0d6d5e45043586b41f2f04827d81bb432184a8d7
Sara <sararossmay2000@gmail.com>
2025-07-08 18:35:49 +0300

    Remove duplicate files from root directory

--- basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-Addition = number1 + number2
-Substraction = number1 - number2
-Multiplication = number1 * number2
-print("the Addition of ", number1, "and ", number2, "is ", Addition)
-print("the Substraction of", number1, "and", number2, "is", Substraction)
-print("the Multiplication of", number1, "and", number2, "is", Multiplication)
