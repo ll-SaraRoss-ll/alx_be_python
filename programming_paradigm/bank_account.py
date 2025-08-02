c3b2e25a9d75fe706b777fee33baa82f6d14dc24
Sara <sararossmay2000@gmail.com>
2025-08-02 14:16:11 +0300

    Updating programming_paradigm folder to alx be python

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,26 +1,22 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        #initializing a new bank account
-        self.account_balance = initial_balance
+    def __init__(self, initial_balance=0.0):
+        self.account_balance = float(initial_balance)
 
     def deposit(self, amount):
-        #adding amount to the account balance
-        if amount <= 0:
+        if amount > 0:
+            self.account_balance += amount
+        else:
             print("Deposit amount must be positive.")
-            return
-        self.account_balance += amount
 
     def withdraw(self, amount):
-        #deducting the specified amount if suffucuent funds are available
         if amount <= 0:
-            print("Withdraw amount must be positive.")
+            print("Withdrawal amount must be positive.")
             return False
         if self.account_balance >= amount:
             self.account_balance -= amount
             return True
-        else:
-            return False
-    
+        return False
+
     def display_balance(self):
-        #prints the current balance
-        print("Current Balance: ${self.account_balance}")
+        # formats the balance to two decimal places
+        print(f"Current Balance: ${self.account_balance:.2f}")
