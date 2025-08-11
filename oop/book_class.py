0b4716a0217b0792eada04c13bf7c3c325070846
Sara <sararossmay2000@gmail.com>
2025-08-11 14:43:18 +0300

    Updating book_class.py to alx_be_python

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,26 +1,14 @@
 class Book:
-    def __init__(self, title: str, author: str, year: int):
-        """
-        Initialize a Book instance with a title, author, and publication year.
-        """
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
     def __del__(self):
-        """
-        Destructor that notifies when a Book instance is being deleted.
-        """
         print(f"Deleting {self.title}")
 
     def __str__(self) -> str:
-        """
-        Human-readable string representation.
-        """
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self) -> str:
-        """
-        Official string representation that can be used to recreate the instance.
-        """
         return f"Book('{self.title}', '{self.author}', {self.year})"
